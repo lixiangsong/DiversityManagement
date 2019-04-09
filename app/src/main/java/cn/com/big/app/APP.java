f327fbc12181d24bb10f6f7c30989b2f8a6aaaf1
LyhAndroid <13427740478@163.com>
2019-04-09 18:31:46 +0800

    x5的初始化

--- app/src/main/java/cn/com/big/app/APP.java
+++ app/src/main/java/cn/com/big/app/APP.java
@@ -2,6 +2,11 @@ package cn.com.big.app;
 
 import android.app.Application;
 
+import com.tencent.smtt.export.external.TbsCoreSettings;
+import com.tencent.smtt.sdk.QbSdk;
+
+import java.util.HashMap;
+
 /**
  * 程序入口.
  *
@@ -15,5 +20,20 @@ public class APP extends Application {
     public void onCreate() {
         super.onCreate();
 
+        initX5WebView();
+    }
+
+    /**
+     * 初始化X5的设置
+     */
+    private void initX5WebView() {
+        // 在调用TBS初始化、创建WebView之前进行如下配置, 以开启优化方案(仅Android 5.1+生效)
+        HashMap<String, Object> map = new HashMap<>();
+        map.put(TbsCoreSettings.TBS_SETTINGS_USE_SPEEDY_CLASSLOADER, true);
+        QbSdk.initTbsSettings(map);
+        // x5内核初始化接口
+        QbSdk.initX5Environment(getApplicationContext(), null);
+        // 非wifi网络条件下是否允许下载内核，默认为false(针对用户没有安装微信/手Q/QQ空间[无内核]的情况下)
+        QbSdk.setDownloadWithoutWifi(true);
     }
 }
