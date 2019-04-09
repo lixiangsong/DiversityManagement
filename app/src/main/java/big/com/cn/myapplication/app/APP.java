ce4cf143e5321741e7e0c610a302530cf3b89ef2
Administrator <13427740478@163.com>
2019-04-09 17:26:19 +0800

    程序入口的配置

---
+++ app/src/main/java/big/com/cn/myapplication/app/APP.java
@@ -0,0 +1,19 @@
+package big.com.cn.myapplication.app;
+
+import android.app.Application;
+
+/**
+ * 程序入口.
+ *
+ * @author luyh
+ * @version 1.0
+ * @date 2019/04/09
+ */
+public class APP extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
